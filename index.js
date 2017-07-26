1f75fe0f5af0caa7a6c37803c4a99da79b7c0ffd
Byron Wright <bslayerw@gmail.com>
2017-07-25 22:31:54 -0400

    added additional tests.

--- index.js
+++ index.js
@@ -2,37 +2,55 @@
 
 // specific the intrepret for running all globally if installed via npm install
 // -g ES6-compliant code
-'use strict';
+"use strict";
 
-const chalk = require('chalk');
-const clear = require('clear');
-const CLI = require('clui');
-const figlet = require('figlet');
-const inquirer = require('inquirer');
+const chalk = require("chalk");
+const clear = require("clear");
+const CLI = require("clui");
+const clc = require("cli-color");
+const figlet = require("figlet");
+const inquirer = require("inquirer");
 const Spinner = CLI.Spinner;
-const _ = require('lodash');
-const fs = require('fs');
-const program = require('commander');
-var packageInfo = require('./package.json');
+const _ = require("lodash");
+const fs = require("fs");
+const program = require("commander");
+var packageInfo = require("./package.json");
 
-const Calculator = require('./lib/calculator')
+const Calculator = require("./lib/calculator");
 
 clear();
-console.log(chalk.yellow(figlet.textSync('Cmd Calculator', {horizontalLayout: 'full'})));
+console.log(
+  chalk.yellow(figlet.textSync("Cmd Calculator", { horizontalLayout: "full" }))
+);
 
-program
-    .version(packageInfo.version)
-    .parse(process.argv); // end with parse to parse through the input
+program.version(packageInfo.version).parse(process.argv); // end with parse to parse through the input
 
-program.on('--help', function () {
-    console.log('  Examples:');
-    console.log('');
-    console.log('    $ custom-help --help');
-    console.log('    $ custom-help -h');
-    console.log('');
+program.on("--help", function() {
+  console.log("  Examples:");
+  console.log("");
+  console.log("    $ custom-help --help");
+  console.log("    $ custom-help -h");
+  console.log("");
 });
-//console.log(`program options ${program}`)
 
 const Calc = new Calculator();
+new Promise(function(resolve) {
+  var result = Calc.evaluate("(1 + 1) * 10");
+  resolve(result);
+}).then(function(result) {
+  console.log(result);
+});
 
-console.log(`calc = ${Calc._infixToPostfix('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3')}`);
+var promise = [];
+for (var i = 0; i < 20; i++) {
+  promise.push(
+    new Promise(function(resolve) {
+      var result = Calc.evaluate(`(1 + ${i}) * 10`);
+      resolve(result);
+    })
+  );
+}
+Promise.all(promise).then(values => {
+    console.log(values);
+})
+
